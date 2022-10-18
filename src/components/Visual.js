8b96a42084531e9eac5e36645cdabc55921e7bfc
tubi55 <tubi55@nate.com>
2022-10-19 00:18:20 +0900

    기본 스크롤 모션

--- src/components/Visual.js
+++ src/components/Visual.js
@@ -1,45 +1,25 @@
 import Anime from '../assets/Anime';
-import { useRef } from 'react';
 
 const btnStyle = {
 	position: 'absolute',
 	top: 120,
 	left: 100,
 };
-const boxStyle = {
-	width: 100,
-	height: 100,
-	backgroundColor: 'aqua',
-	position: 'absolute',
-	top: 200,
-	left: 200,
-};
 
 function Visual() {
-	const box = useRef(null);
-
 	return (
 		<figure id='visual'>
 			<button
 				style={btnStyle}
 				onClick={() => {
-					new Anime(box.current, {
-						prop: 'left',
-						value: 1000,
-						duration: 500,
-						callback: () => {
-							new Anime(box.current, {
-								prop: 'top',
-								value: 500,
-								duration: 1000,
-							});
-						},
+					new Anime(window, {
+						prop: 'scroll',
+						value: 3000,
+						duration: 1000,
 					});
 				}}>
 				start
 			</button>
-
-			<div className='box' style={boxStyle} ref={box}></div>
 		</figure>
 	);
 }
