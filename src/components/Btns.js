6b7ab13d9362e0f65cf122796e1f4cec70f95205
tubi55 <tubi55@nate.com>
2022-10-19 09:48:14 +0900

    box활성화

--- src/components/Btns.js
+++ src/components/Btns.js
@@ -1,8 +1,10 @@
 import { useRef, useEffect } from 'react';
+import Anime from '../assets/Anime';
 
 function Btns({ setPos, setScrolled }) {
 	const pos = useRef([]);
 	const btnRef = useRef(null);
+	const speed = 500;
 
 	//각 섹션의 세로위치값 구하는 함수
 	const getPos = () => {
@@ -16,19 +18,24 @@ function Btns({ setPos, setScrolled }) {
 	const activation = () => {
 		const base = -window.innerHeight / 4;
 		const btns = btnRef.current.children;
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
 		const scroll = window.scrollY;
 		setScrolled(scroll);
 
 		pos.current.map((pos, idx) => {
 			if (scroll >= pos + base) {
 				for (const btn of btns) btn.classList.remove('on');
+				for (const sec of secs) sec.classList.remove('on');
 				btns[idx].classList.add('on');
+				secs[idx].classList.add('on');
 			}
 		});
 	};
 
 	//컴포넌트가 처음 마운트시 getPos함수를 resize이벤트에 연결
 	useEffect(() => {
+		btnRef.current.children[0].classList.add('on');
+		btnRef.current.parentElement.querySelectorAll('.myScroll')[0].classList.add('on');
 		getPos();
 		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', activation);
@@ -44,7 +51,15 @@ function Btns({ setPos, setScrolled }) {
 			{Array(4)
 				.fill()
 				.map((_, idx) => (
-					<li key={idx}></li>
+					<li
+						key={idx}
+						onClick={() => {
+							new Anime(window, {
+								prop: 'scroll',
+								value: pos.current[idx],
+								duration: speed,
+							});
+						}}></li>
 				))}
 		</ul>
 	);
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,10 @@ function News() {
 	return (
 		<section id='news' className='myScroll'>
 			<h1>News</h1>
+			<article></article>
+			<article></article>
+			<article></article>
+			<article></article>
 		</section>
 	);
 }
